d7081685a7d6f4f917b1ea5a7ff3b7dc86d2229d
Fabián Ardila <fabianra97@gmail.com>
2021-09-20 09:00:20 -0500

    :sparkles: Refactor file structure to order project

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='margen'>
+      <table className='tabla'>
+      <thead>
+        <tr>
+          <th>
+            Nombre
+          </th>
+          <th>
+            Correo
+          </th>
+          <th>
+            Enlace
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            Fabián
+          </td>
+          <td>
+            fabianra97@gamil.com
+          </td>
+          <td>
+            ardilafabian.com
+          </td>
+        </tr>
+      </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
