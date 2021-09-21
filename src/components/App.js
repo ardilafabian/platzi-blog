45391569eef7d21feff6aeffa72f01aa67a21c99
Fabián Ardila <fabianra97@gmail.com>
2021-09-21 07:53:44 -0500

    :sparkles: Change app from stateless to stateful

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  return (
-    <div className='margen'>
-      <table className='tabla'>
-      <thead>
-        <tr>
-          <th>
-            Nombre
-          </th>
-          <th>
-            Correo
-          </th>
-          <th>
-            Enlace
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            Fabián
-          </td>
-          <td>
-            fabianra97@gamil.com
-          </td>
-          <td>
-            ardilafabian.com
-          </td>
-        </tr>
-      </tbody>
-      </table>
-    </div>
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      usuarios: [
+        {
+          nombre: 'Fabian',
+          correo: 'fardila@mail.com',
+          enlace: 'fabian.com'
+        },
+        {
+          nombre: 'Carlos',
+          correo: 'cardlos@mail.com',
+          enlace: 'fabian.com'
+        }
+      ]
+    }
+  }
+  
+  ponerFilas = () => (
+    this.state.usuarios.map((usuario) => (
+      <tr>
+        <td>
+          { usuario.nombre }
+        </td>
+        <td>
+          { usuario.correo }
+        </td>
+        <td>
+          { usuario.enlace }
+        </td>
+      </tr>
+    ))
   );
+
+  render() {
+    return (
+      <div className='margen'>
+        <table className='tabla'>
+        <thead>
+          <tr>
+            <th>
+              Nombre
+            </th>
+            <th>
+              Correo
+            </th>
+            <th>
+              Enlace
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.ponerFilas()}
+        </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
